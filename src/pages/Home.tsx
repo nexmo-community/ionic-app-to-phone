527f692f0ac5c66f705952d63a7c9e3c4a0093b3
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2021-04-28 12:04:16 +0100

    app to phone web

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,97 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton} from '@ionic/react';
+import nexmoClient, { Application, NXMCall } from 'nexmo-client';
+import React from 'react';
 import './Home.css';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+class Home extends React.Component<{}, { application: Application | null, call: NXMCall | null, buttonText: string, buttonAction: any, clientState: string, callStatus: string }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      application: null,
+      call: null,
+      buttonText: "Login",
+      buttonAction: () => this.login("ALICE_JWT"),
+      clientState: "Unknown",
+      callStatus: "Idle",
+    };
+  }
+
+  login(jwt: string) {
+    let nexmo = new nexmoClient();
+    nexmo.login(jwt).then(app => {
+      this.setState({ 
+        application: app,
+        buttonText: "Call",
+        clientState: "Connected",
+        buttonAction: () => this.makeCall()
+       });
+    });
+  }
+
+  makeCall() {
+    this.state.application?.callServer("").then((call) => {
+      this.setState({
+        callStatus: "On Call",
+        buttonText: "End Call",
+        buttonAction: () => this.endCall(),
+        call: call
+      })
+    }).catch((error) => {
+      console.error(error);
+    });
+
+    this.state.application?.on("call:status:changed", (nxmCall: NXMCall) => {
+      if (nxmCall.status === nxmCall.CALL_STATUS.COMPLETED) {
+        this.resetCallState();
+      }
+    });
+  }
+
+  endCall() {
+    this.state.call?.hangUp().then(() => {
+      this.resetCallState();
+    });
+  }
+
+  resetCallState() {
+    this.setState({
+      callStatus: "Idle",
+      buttonText: "Call",
+      buttonAction: () => this.makeCall(),
+      call: null
+    });
+  }
+
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Vonage App</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Vonage app</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <div className="text">
+          {this.state.clientState}
+            </div>
+          <div className="container">
+            <div className="text">
+              Call Status: {this.state.callStatus}
+            </div>
+            <IonButton expand="block" onClick={this.state.buttonAction}>
+              {this.state.buttonText}
+            </IonButton>
+          </div>
+
+        </IonContent>
+      </IonPage>
+    );
+  };
+}
 
 export default Home;
